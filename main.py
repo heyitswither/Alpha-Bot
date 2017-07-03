55d554a7bb0b074dfacc619e76cb647ecd50e13f
Tucker Boniface <Bonifacetucker@gmail.com>
2017-07-03 12:43:37 -0700

    import cogs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 import sys
+import os
 
 bot = discord.Client()
 
@@ -22,6 +23,28 @@ def import_config():
       json.dump(config_temp, file_out, indent=2, sort_keys=True)
     print("Please put your bot's token in the 'token' key in config.json")
     sys.exit()
+
+def add_cogs():
+  startup_extensions = []
+  for cog in os.popen('ls cogs/').split('\n'):
+    startup_extensions.append("cogs." + cog.split('.')[0])
+  return startup_extensions
+
+@bot.event
+async def on_ready():
+  print('Logged in as')
+  print(bot.user.name)
+  print(bot.user.id)
+  print('------')
+
+  await bot.change_presence(game=discord.Game(name='"[help" for help'))
+
+  for extension in add_cogs():
+    try:
+      bot.load_extension(extension)
+    except Exception as e:
+      exc = '{}: {}'.format(type(e).__name__, e)
+      print('Failed to load extension {}\n{}'.format(extension, exc))
 
 if __name__ == '__main__':
   import_config()
