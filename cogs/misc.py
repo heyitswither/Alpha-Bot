e9af91fd8e981b0d24c18ce551ad1f76f779f3fa
Tucker Boniface <Bonifacetucker@gmail.com>
2017-07-07 18:17:08 -0700

    nice

--- cogs/misc.py
+++ cogs/misc.py
@@ -85,7 +85,7 @@ class Misc:
     """
     suggestion = ' '.join(suggestion)
     embed = discord.Embed(title="New Suggestion", description=suggestion)
-    embed.set_author(name=ctx.message.author.name + "#" + ctx.message.author.discriminator + "(" + ctx.message.author.id + ")")
+    embed.set_author(name=ctx.message.author.name + "#" + ctx.message.author.discriminator + " (" + ctx.message.author.id + ")", icon_url=ctx.message.author.avatar_url)
     await self.bot.send_message(self.bot.get_server('197780624688414720').get_channel('332616763294613505'), embed=embed)
 
 def setup(bot):
